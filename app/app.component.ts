c882e25c54c2e8a247ccf5d8c3ee40afaf9fc33b
PLINIORZAVALA <134644204+PLINIORZAVALA@users.noreply.github.com>
2024-08-20 13:42:00 -0500

    0.4 commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet,RouterLink } from '@angular/router';
 import { userComponent } from './components/user/user.component';
+import { userSerrvice } from './services/user.service';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,userComponent],
+  imports: [
+    RouterOutlet,
+    userComponent,
+    RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'frondEssalud';
+export class AppComponent implements OnInit{
+  public data: any;
+
+  //Inyectamos los serviccios en el constructor
+  constructor(private API: userSerrvice){}
+
+  ngOnInit(): void {
+    //Llamanos al servicio para obtener los datos del usurario
+    this.API.getUser().subscribe(
+      response => {
+        this.data = response;
+        console.log(this.data);
+      },
+      error => {
+        console.log(error)
+      }
+    )
+  }
 }
